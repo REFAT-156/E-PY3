1f6adc23967d5903739b4a44177f9b657d333151
FAHAD HOSSAIN REFAT <109792845+REFAT-156@users.noreply.github.com>
2022-08-21 14:47:01 +0600

    Create Enc.py

---
+++ Enc.py
@@ -0,0 +1,18 @@
+import os, platform, time
+print('\n\x1b[1;37m[•] Checking Update...');time.sleep(0.5)
+os.system('git pull')
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            print("\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
+            print('[•] Follow My Github First')
+            os.system('xdg-open https://github.com/REFAT-156')
+            import enc6
+        elif bit == '32bit':
+            print("\n\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
+            print('[•] Follow My Github First')
+            os.system('xdg-open https://github.com/REFAT-156')
+            import enc3
+        else:
+            exit('\033[1;31m[×] Connection Error')
+out()
